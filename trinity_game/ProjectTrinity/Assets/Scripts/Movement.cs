1c67fcc06c73cae52628892b68973743982b3041
AIvanov352 <alexivanov307@gmail.com>
2022-02-21 19:26:02 +0200

    Half functional power Ups
    Made timer
    and others

--- trinity_game/ProjectTrinity/Assets/Scripts/Movement.cs
+++ trinity_game/ProjectTrinity/Assets/Scripts/Movement.cs
@@ -8,6 +8,12 @@ public class Movement : MonoBehaviour
 
     public GameObject player;
 
+    private Rigidbody2D playerRigid;
+
+    private platformCreator pCreator;
+
+    public GameObject platforms;
+
     public bool onPlatform = false;
 
     private int platformNum;
@@ -18,14 +24,35 @@ public class Movement : MonoBehaviour
     
     public float pointsIncreasedPerPlatform;
 
+    public float timeRemaining;
+
+    public bool controllable = true;
+
+
+    void Awake()
+    {
+        pCreator = platforms.GetComponent<platformCreator>();
+    }
+
     void Update()
     {
         scoringText.text = (int)scoreAmount + "";
-
-
+        playerRigid = GetComponent<Rigidbody2D>();
+        if(controllable == false)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                fixBoost();
+            }
+        }
     }
 
     public void MoveLeft() {
+        if(controllable == true) { 
         GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platforms");
         float minDist = Mathf.Infinity;
         platformNum = -100;
@@ -47,30 +74,34 @@ public class Movement : MonoBehaviour
             + player.transform.localScale.y / 2);
 
         platforms = null; //delete all objects inside
+        }
      }
 
     public void MoveRight() {
-        GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platforms");
-        float minDist = Mathf.Infinity;
-        platformNum = -100;
-        for (int i = 0; i < platforms.Length; i++) //platform i
+        if (controllable == true)
         {
-            if (platforms[i].transform.position.y > player.transform.position.y
-                && platforms[i].transform.position.x > 0) //check if its above player and on right
+            GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platforms");
+            float minDist = Mathf.Infinity;
+            platformNum = -100;
+            for (int i = 0; i < platforms.Length; i++) //platform i
             {
-                float dist = Vector3.Distance(platforms[i].transform.position, player.transform.position);
-                if (dist < minDist)
+                if (platforms[i].transform.position.y > player.transform.position.y
+                    && platforms[i].transform.position.x > 0) //check if its above player and on right
                 {
-                    minDist = dist;
-                    platformNum = i;
+                    float dist = Vector3.Distance(platforms[i].transform.position, player.transform.position);
+                    if (dist < minDist)
+                    {
+                        minDist = dist;
+                        platformNum = i;
+                    }
                 }
             }
-        }
-        player.transform.position = new Vector2(platforms[platformNum].transform.position.x
-            , platforms[platformNum].transform.position.y + platforms[platformNum].transform.localScale.y / 2  //magic happens
-            + player.transform.localScale.y / 2);
+            player.transform.position = new Vector2(platforms[platformNum].transform.position.x
+                , platforms[platformNum].transform.position.y + platforms[platformNum].transform.localScale.y / 2  //magic happens
+                + player.transform.localScale.y / 2);
 
-        platforms = null; //delete all objects inside
+            platforms = null; //delete all objects inside
+        }
     }
 
         private void OnCollisionEnter2D(Collision2D collision)
@@ -84,19 +115,23 @@ public class Movement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Boost")
+        if (collision.gameObject.tag == "Boost") // TODO: 1) freeze player 2) platforms faster 
         {
             Debug.Log("Boosting NOW!!!!");
+            playerRigid.constraints = RigidbodyConstraints2D.FreezeAll;
+            player.transform.GetComponent<BoxCollider2D>().enabled = false;
+            controllable = false;
+            timeRemaining = 3;
+            //platforms.GetComponent<platformCreator>().current_platform_speed = 15.0f;
         }
-        else if (collision.gameObject.tag == "Invincible")
+        else if (collision.gameObject.tag == "Invincible") //TODO: Timer
         {
             Debug.Log("Invincible NOW!!!");
         }
-        else if (collision.gameObject.tag == "DoublePoints")
+        else if (collision.gameObject.tag == "DoublePoints") //TODO: Timer
         {
             Debug.Log("DoulePoints NOW!!!");
         }
-
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -107,4 +142,13 @@ public class Movement : MonoBehaviour
             onPlatform = false;
         }
     }
+
+    private void fixBoost()
+    {
+        controllable = true;
+        playerRigid.constraints = RigidbodyConstraints2D.None;
+        player.transform.GetComponent<BoxCollider2D>().enabled = true;
+        //platforms.GetComponent<platformCreator>().current_platform_speed = 6.0f;
+    }
+
 }
